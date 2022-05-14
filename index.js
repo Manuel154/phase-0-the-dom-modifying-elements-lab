fec6755398f26fc75aa8a1d49b5237bf05d0109d
Manuel <manueldaeira2@gmail.com>
2022-05-14 12:33:47 -0400

    commit tree e44906267589f4d4e1867a3a5b312628f8deee97

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const removing = document.getElementById('main');
+removing.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.textContent = " Manny is the champion"
